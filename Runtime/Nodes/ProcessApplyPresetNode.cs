e8a78dd9719677476d144817165ab0df0995896b
mitay-walle <mitay-walle91@mail.ru>
2022-06-14 17:41:46 +0300

    #if fixes

--- Runtime/Nodes/ProcessApplyPresetNode.cs
+++ Runtime/Nodes/ProcessApplyPresetNode.cs
@@ -1,48 +1,55 @@
 using System;
 using GraphProcessor;
 using UnityEditor;
-using UnityEditor.Presets;
 using UnityEngine;
 using Object = UnityEngine.Object;
+#if UNITY_EDITOR
+using UnityEditor.Presets;
+#endif
+
 namespace mitaywalle.AssetProcessGraph.Nodes
 {
-	[Serializable, NodeMenuItem("Process/Apply Preset", typeof(AssetProcessGraph))]
-	public class ProcessApplyPresetNode : ProcessImporterNode
-	{
-		public override string name => $"Apply Preset {Preset?.name}";
-		[Input("Assets")] protected Object[] assets;
-		[Output("Assets")] protected Object[] outputAssets;
-		[SerializeField] Preset Preset;
+    [Serializable, NodeMenuItem("Process/Apply Preset", typeof(AssetProcessGraph))]
+    public class ProcessApplyPresetNode : ProcessImporterNode
+    {
+        [Input("Assets")] protected Object[] assets;
+        [Output("Assets")] protected Object[] outputAssets;
+#if UNITY_EDITOR
+        [SerializeField] Preset Preset;
+        public override string name => $"Apply Preset {Preset?.name}";
+#endif
+        protected override void Process()
+        {
+            base.Process();
+            if (assets != null)
+            {
+                foreach (Object asset in assets)
+                {
+                    ProcessAsset(asset);
+                }
+            }
 
-		protected override void Process()
-		{
-			base.Process();
-			if (assets != null)
-			{
-				foreach (Object asset in assets)
-				{
-					ProcessAsset(asset);
-				}	
-			}
-			
+            outputAssets = assets;
+        }
 
-			outputAssets = assets;
-		}
+        protected void ProcessAsset(Object asset)
+        {
+#if UNITY_EDITOR
+            if (Preset.CanBeAppliedTo(asset))
+            {
+                Preset.ApplyTo(asset);
+            }
+#endif
+        }
 
-		protected void ProcessAsset(Object asset)
-		{
-			if (Preset.CanBeAppliedTo(asset))
-			{
-				Preset.ApplyTo(asset);
-			}
-		}
-
-		protected override void ProcessImporter(AssetImporter importer)
-		{
-			if (Preset.CanBeAppliedTo(importer))
-			{
-				Preset.ApplyTo(importer);
-			}
-		}
-	}
+#if UNITY_EDITOR
+        protected override void ProcessImporter(AssetImporter importer)
+        {
+            if (Preset.CanBeAppliedTo(importer))
+            {
+                Preset.ApplyTo(importer);
+            }
+        }
+#endif
+    }
 }
